0d5fb1d5e965da099854b553e1fa46b191547502
Mohsin <mohsinarifpcc@gmail.com>
2023-06-06 22:31:30 +0530

    C6:Increase api request timeout

--- app/src/main/java/com/example/openindemo/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/openindemo/api/RetrofitInstance.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
 
@@ -15,6 +16,9 @@ class RetrofitInstance {
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
             val client = OkHttpClient.Builder()
                 .addInterceptor(logging)
+                .callTimeout(20,TimeUnit.SECONDS)
+                .readTimeout(20,TimeUnit.SECONDS)
+                .writeTimeout(20,TimeUnit.SECONDS)
                 .build()
 
             Retrofit.Builder()
